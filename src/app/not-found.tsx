cb57caecf3a815447e86b60416128c0004f9ad1a
Youssef Abdelrahim <youssefezzatg@gmail.com>
2025-10-29 07:06:06 +0100

    update not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export default function NotFound() {
   return (
-    <div className="flex h-52 w-full items-center justify-center">
-      <p className="text-xl text-center">
+    <div className="flex h-52 w-full gap-8 flex-col items-center justify-center">
+      <Image
+        width={100}
+        height={100}
+        src="/assets/very-good.gif"
+        alt="A very good GIF illustrating the error."
+      />
+      <p className="text-center text-xl">
         [404] Dear brain user, now I give you permission to use the navigation
         bar.
       </p>
